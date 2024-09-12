f5bb0fe275e729114912dd9e7fc8d61434f23704
Евгений Кузнецов <egions2006@gmail.com>
2024-09-12 20:25:25 +0300

    few corrections

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,4 +1,6 @@
-﻿public class Program
+﻿namespace Lab3;
+
+public class Program
 {
     /// <summary>
     /// Максимальное количество выступающих пар.
@@ -8,7 +10,7 @@
     public static void Main()
     {
         // Словарь для хранения количества голосов за каждую пару
-        Dictionary<int, int> votes = new();
+        Dictionary<int, int> votes = [];
 
         for (int i = 1; i <= MAX_PAIRS; i++)
             votes.Add(i, 0);
@@ -35,9 +37,9 @@
 
             var pairNumber = random.Next(1, MAX_PAIRS + 1);
 
-            if (votes.ContainsKey(pairNumber)) 
+            if (votes.ContainsKey(pairNumber))
                 votes[pairNumber]++;
-            else 
+            else
                 votes.Add(pairNumber, 1);
         }
         #endregion
@@ -45,11 +47,12 @@
         // Сортировка по количеству голосов в порядке убывания
         var sortedVotes = votes.OrderByDescending(pair => pair.Value).ToDictionary();
 
-        Console.WriteLine("\nРезультаты голосования");
-        Console.WriteLine("№ пары\t| Голоса");
+        Console.WriteLine(
+            "\nРезультаты голосования\n" +
+            "№ пары\t| Голоса"
+        );
+
         foreach (var pair in sortedVotes)
-        {
             Console.WriteLine($"{pair.Key}\t- {pair.Value}");
-        }
     }
 }
