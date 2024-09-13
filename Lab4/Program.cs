40c348f2cb7b646f1575df3ac4f3411bcac4354a
Евгений Кузнецов <egions2006@gmail.com>
2024-09-13 18:32:39 +0300

    сделана лабораторная №4

---
+++ Lab4/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace Lab4;
+
+class Program
+{
+    static void Main()
+    {
+        #region Ввод чисел
+        int first;
+        while (true)
+        {
+            Console.Write("- Число 1: ");
+
+            if (!int.TryParse(Console.ReadLine(), out var parsedNumber))
+                continue;
+
+            first = parsedNumber;
+            break;
+        }
+
+        int second;
+        while (true)
+        {
+            Console.Write("- Число 2: ");
+
+            if (!int.TryParse(Console.ReadLine(), out var parsedNumber))
+                continue;
+
+            second = parsedNumber;
+            break;
+        }
+        #endregion
+
+        // Обычное умножение
+        int usual = first * second;
+
+        #region Установка флагов, перевод отрицательных чисел в положительные
+        bool isFirstNeg = false;
+        bool isSecondNeg = false;
+        if (first < 0)
+        {
+            isFirstNeg = true;
+            first = ~first + 1;
+        }
+        if (second < 0)
+        {
+            isSecondNeg = true;
+            second = ~second + 1;
+        }
+        #endregion
+
+        int recursive = RecursiveMultiplication(first, second);
+        int binary = BinaryMultiplication(first, second);
+
+        #region Инверсия ответов, если установлены флаги отрицательных чисел
+        if (isFirstNeg)
+        {
+            recursive = ~recursive + 1;
+            binary = ~binary + 1;
+        }
+        if (isSecondNeg)
+        {
+            recursive = ~recursive + 1;
+            binary = ~binary + 1;
+        }
+        #endregion
+
+        Console.WriteLine(
+            $"Рекурсией:\t{recursive}\n" +
+            $"Двоичное:\t{binary}\n" +
+            $"Обычное:\t{usual}"    
+        );
+    }
+
+    /// <summary>
+    /// Рекурсивное умножение.
+    /// </summary>
+    static int RecursiveMultiplication(int a, int b)
+    {
+        if (b == 0) return 0;
+
+        return a + RecursiveMultiplication(a, --b);
+    }
+
+    /// <summary>
+    /// Двоичное умножение.
+    /// </summary>
+    static int BinaryMultiplication(int a, int b)
+    {
+        var result = 0;
+        while (b > 0)
+        {
+            if ((b & 1) == 1) // Проверка нечетности b
+                result += a;
+
+            a <<= 1; // Удвоение a
+            b >>= 1; // Деление b на 2
+        }
+        return result;
+    }
+}
