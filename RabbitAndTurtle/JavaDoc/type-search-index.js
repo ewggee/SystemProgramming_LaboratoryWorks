10fbb349cb1834c33f80d83682e74bb00df57b04
Евгений Кузнецов <egions2006@gmail.com>
2024-11-09 11:53:27 +0300

    удалил HTML JavaDoc

--- RabbitAndTurtle/JavaDoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"AnimalThread"},{"p":"<Unnamed>","l":"RabbitAndTurtle"}];updateSearchResults();
