f6876e435ff875433703d0d7c25712f2d4c03596
Евгений Кузнецов <egions2006@gmail.com>
2024-11-15 20:13:10 +0300

    сделал лабораторную работу 13

---
+++ Lab13/ABC/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ABC;
+
+class Program
+{
+    const string FILE_PATH = "fl.txt";
+    readonly static string Sequence =
+        File.ReadAllText(FILE_PATH)
+        .Replace("AB", ".")
+        .Replace("CB", ".");
+
+    static void Main()
+    {
+        var max = Regex
+            .Split(Sequence, "\\w")
+            .Max().Length;
+
+        Console.WriteLine(max);
+    }
+}
