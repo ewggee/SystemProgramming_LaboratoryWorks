18afb3aacd326bee17664b2f96f4b4fbf8c99d65
Евгений Кузнецов <egions2006@gmail.com>
2024-11-15 19:35:32 +0300

    сделал лабораторную 13

---
+++ Lab13/QRW123/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace QRW123;
+
+class Program
+{
+    const string FILE_PATH = "fl.txt";
+    readonly static string Sequence = File.ReadAllText(FILE_PATH);
+
+    static void Main()
+    {
+        var max = 0;
+        var current = 0;
+        for (int i = 1; i < Sequence.Length; i++)
+        {
+            if (char.IsLetter(Sequence[i]) != char.IsLetter(Sequence[i - 1]))
+            {
+                current++;
+            }
+            else
+            {
+                max = Math.Max(max, current);
+                current = 0;
+            }
+        }
+
+        Console.WriteLine(Math.Max(max, current));
+    }
+}
